d5efa59b6eb693240ee078c273d206d4ebfca805
Leo jPod <leo.jpod@lilo.org>
2020-08-17 16:08:52 +0200

    now code it

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,9 +1,10 @@
-module Main exposing (main)
+module Main exposing (Board, Cell(..), Group, computeGroups, main)
 
 import Browser
 import Html exposing (Html, div, span, table, td, text, tr)
 import Html.Attributes exposing (class)
 import Html.Attributes.Extra
+import List
 import Random
 
 
@@ -44,6 +45,67 @@ type alias Point =
 
 type alias Group =
     List Point
+
+
+isConnectedGroup : Int -> Int -> Group -> Bool
+isConnectedGroup x y =
+    List.any
+        (\( x_, y_ ) ->
+            (x == x_ && (y_ == y + 1 || y_ == y - 1))
+                || (y == y_ && (x_ == x + 1 || x_ == x - 1))
+        )
+
+
+partitionGroups : Int -> Int -> List Group -> ( List Group, List Group )
+partitionGroups x y =
+    List.foldl
+        (\group ( connected, notConnected ) ->
+            if isConnectedGroup x y group then
+                ( group :: connected, notConnected )
+
+            else
+                ( connected, group :: notConnected )
+        )
+        ( [], [] )
+
+
+computeGroups : Board -> List Group
+computeGroups board =
+    --| step 1: from matrix to list of things
+    board
+        |> List.indexedMap
+            (\x ->
+                List.indexedMap
+                    (\y cell ->
+                        if cell == Fill then
+                            Just ( x, y )
+
+                        else
+                            Nothing
+                    )
+            )
+        |> List.concatMap identity
+        |> List.filterMap identity
+        --| step 2 reduce that list according to the following
+        --| for each filled up cell, find all the groups that connect to it:
+        --| if there are none, create one with the cell in items--| if there is just one, add the cell to it
+        --| if there are more than one then merge them
+        |> List.foldl
+            (\( x, y ) groups ->
+                partitionGroups x y groups
+                    |> (\( connectedGroups, notConnectedGroups ) ->
+                            case connectedGroups of
+                                [] ->
+                                    [ ( x, y ) ] :: groups
+
+                                _ ->
+                                    (( x, y ) :: List.concat connectedGroups) :: notConnectedGroups
+                       )
+            )
+            []
+        --| NOTE: the sorting is mostly just so that the test pass nicely
+        |> List.map List.sort
+        |> List.sort
 
 
 type alias Model =
